8e89f23aa227111e6eb9f58261a6cbb1b6695327
sadakchap <aliceprerna@gmail.com>
2020-12-22 19:33:33 +0530

    created authContext

---
+++ src/contexts/auth.js
@@ -0,0 +1,60 @@
+import { createContext, useReducer } from "react";
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+  user: null,
+};
+
+if (localStorage.getItem("jwt")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwt"));
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwt");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
+
+export const AuthContext = createContext({
+  user: null,
+  login: (userData) => {},
+  logout: () => {},
+});
+// userData: {username: "", id: "", token: ""}
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
+const AuthContextProvider = (props) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  const login = (userData) => {
+    localStorage.setItem("jwt", userData.token);
+    dispatch({
+      type: "LOGIN",
+      payload: userData,
+    });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    dispatch({
+      type: "LOGOUT",
+    });
+  };
+
+  return (
+    <AuthContext.Provider value={{ ...state, login, logout }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;

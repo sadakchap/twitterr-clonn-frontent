2395cafe6787a0af95513ae79962aacaf20a185f
sadakchap <aliceprerna@gmail.com>
2020-12-22 19:32:55 +0530

    integrated apollo client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
+import ApolloProvider from "./components/ApolloProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

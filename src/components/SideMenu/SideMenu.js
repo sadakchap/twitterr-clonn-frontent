917875c2a818c5d503318103b79d43bdbcdc7227
sadakchap <aliceprerna@gmail.com>
2020-12-23 07:59:46 +0530

    created Side Menu

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,88 @@
+import {
+  Drawer,
+  Hidden,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  useTheme,
+} from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
+
+const SideMenu = (props) => {
+  const { window, mobileOpen, handleDrawerToggle } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const drawer = (
+    <div>
+      <List>
+        <ListItem>
+          <TwitterIcon style={{ fontSize: "2rem" }} />
+        </ListItem>
+        <ListItem button component={NavLink} to="/home">
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <nav className={classes.drawer} aria-label="mailbox folders">
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Hidden smUp implementation="css">
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor={theme.direction === "rtl" ? "right" : "left"}
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <Drawer
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          variant="permanent"
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+    </nav>
+  );
+};
+
+export default SideMenu;

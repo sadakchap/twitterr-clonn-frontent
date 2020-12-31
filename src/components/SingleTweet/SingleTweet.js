3e2fbc93d32b7f66672eb04909d3dd39bfd38faa
sadakchap <aliceprerna@gmail.com>
2020-12-31 16:01:04 +0530

    displaying single tweet

--- src/components/SingleTweet/SingleTweet.js
+++ src/components/SingleTweet/SingleTweet.js
@@ -1,8 +1,23 @@
 import Base from "../../components/Base/Base";
-import { IconButton, makeStyles, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  Divider,
+  IconButton,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import ExploreSection from "../../components/ExploreSection/ExploreSection";
-import Tweet from "../Tweet/Tweet";
 import ArrowBackOutlinedIcon from "@material-ui/icons/ArrowBackOutlined";
+import TweetHeader from "../Tweet/TweetHeader";
+import DisplayTweetMsg from "../Tweet/DisplayTweetMsg";
+import moment from "moment";
+import TweetActions from "../Tweet/TweetActions";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,11 +51,65 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   toolBar: theme.mixins.toolbar,
+  tweetCard: {
+    flexGrow: 1,
+    padding: `${theme.spacing(2)}px`,
+    display: "flex",
+    width: "100%",
+    border: "none",
+    borderBottom: `1px solid ${theme.palette.grey[700]}`,
+    backgroundColor: `${theme.palette.background.default}`,
+  },
+  tweetProfilePic: {
+    width: 50,
+    paddingRight: "4px",
+  },
+  tweetContent: {
+    width: `calc(100% - 50px)`,
+    "& .MuiCardContent-root": {
+      padding: `${theme.spacing(1)}px 0`,
+    },
+  },
+  tweetBody: {
+    padding: `${theme.spacing(1)}px 0 0 !important`,
+  },
+  tweetStatus: {
+    display: "flex",
+    gap: "16px",
+    padding: `${theme.spacing(1)}px 0`,
+    "& div span": {
+      fontWeight: 700,
+      color: "#fff",
+      fontSize: "0.9rem",
+    },
+    "& div": {
+      color: `${theme.palette.text.disabled}`,
+    },
+  },
 }));
 
 const SingleTweet = (props) => {
   const classes = useStyles();
-  console.log(props.location.state.data);
+  const { tweetId } = useParams();
+
+  const {
+    loading,
+    data: {
+      getPost: {
+        id,
+        username,
+        createdAt,
+        commentsCount,
+        likesCount,
+        body,
+        comments,
+        likes,
+        author: { name, profile_pic } = {},
+      } = {},
+    } = {},
+  } = useQuery(FETCH_SINGLE_TWEET_QUERY, {
+    variables: { postId: tweetId },
+  });
 
   return (
     <Base>
@@ -59,13 +128,59 @@ const SingleTweet = (props) => {
                 Tweet
               </Typography>
             </div>
-
-            <div className={classes.latestTweets}>
-              <Tweet
-                tweet={props.location.state.data.tweet}
-                allowCardHover={false}
-              />
-            </div>
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <div className={classes.tweetCardBox}>
+                <Card variant="outlined" className={classes.tweetCard}>
+                  <div className={classes.tweetProfilePic}>
+                    <Avatar src={profile_pic} alt={username} />
+                  </div>
+                  <div className={classes.tweetContent}>
+                    <TweetHeader
+                      postId={id}
+                      name={name}
+                      username={username}
+                      createdAt={createdAt}
+                    />
+                    <CardContent className={classes.tweetBody}>
+                      <DisplayTweetMsg body={body} />
+                    </CardContent>
+                    <CardContent>
+                      <Typography
+                        color="textSecondary"
+                        variant="caption"
+                        component="div"
+                        style={{ margin: "8px 0" }}
+                      >
+                        {moment(new Date(parseInt(createdAt))).format(
+                          "h:mm A . MMM Do, YYYY"
+                        )}
+                      </Typography>
+                      <Divider />
+                      <div className={classes.tweetStatus}>
+                        <div className="">
+                          <span>{likesCount}</span>{" "}
+                          {likesCount === 1 ? "Like" : "Likes"}
+                        </div>
+                        <div className="">
+                          <span> {commentsCount}</span> Quote{" "}
+                          {commentsCount === 1 ? "Tweet" : "Tweets"}
+                        </div>
+                      </div>
+                      <Divider />
+                    </CardContent>
+                    <CardActions style={{ padding: 0 }}>
+                      <TweetActions
+                        tweet={{ id, likes, comments }}
+                        viewTweet={false}
+                      />
+                    </CardActions>
+                  </div>
+                </Card>
+                {/* Reply to tweets */}
+              </div>
+            )}
           </div>
           <div className={classes.exploreSection}>
             <ExploreSection />
@@ -77,3 +192,29 @@ const SingleTweet = (props) => {
 };
 
 export default SingleTweet;
+
+const FETCH_SINGLE_TWEET_QUERY = gql`
+  query getPost($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      username
+      createdAt
+      likesCount
+      commentsCount
+      likes {
+        id
+        username
+      }
+      comments {
+        id
+        body
+        username
+      }
+      author {
+        name
+        profile_pic
+      }
+    }
+  }
+`;
